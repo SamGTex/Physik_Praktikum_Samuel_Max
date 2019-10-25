1e0501397839a7d34cb07b82f924315f7c5fa1fa
Samuel Haefs <samuel.haefs@tu-dortmund.de>
2019-10-25 09:25:38 +0200

    weitere sortierung

--- 802_Fourier/802_Fourier_2.py
+++ 802_Fourier/802_Fourier_2.py
@@ -1,11 +1,20 @@
 import numpy as np
 
 T = np.pi
-for k in range(17):
-    print(4/(np.pi-4*k**2*np.pi))
+for k in range(18):
 
-#obere = 2/T * (np.sign(np.sin(T/2)) * (w * np.sin(T/2) * np.sin(w * T/2) 
-# + np.cos(T/2) * np.cos(w * T/2))) / (w**2 - 1)
-#
-#   untere = 2/T * (np.sign(np.sin(-T/2)) * (w * np.sin(-T/2) * np.sin(w * (-T/2)) 
-#  + np.cos(-T/2) * np.cos(w * (-T/2)))) / (w**2 - 1) https://www.onlinemathe.de/forum/Fourier-Reihe-sinx
+    w=2*np.pi*k/T
+    a=np.pi
+    b=0
+
+    obere_1 = 2/T * (np.sign(np.sin(a)) * (w * np.sin(a) * np.sin(w * a) 
+    + np.cos(a) * np.cos(w * a))) / (w**2 - 1)
+
+    untere_1 = 2/T * (np.sign(np.sin(b)) * (w * np.sin(b) * np.sin(w * b) 
+    + np.cos(b) * np.cos(w * b))) / (w**2 - 1)
+
+    print('w:',w)
+    print('A_1:',obere_1-untere_1)
+    print('A_2:',4/(-4*k**2*np.pi+np.pi))
+
+#https://www.onlinemathe.de/forum/Fourier-Reihe-sinx
