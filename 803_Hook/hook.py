e24b78a48d4cb24d1728eb49564627d06056b262
Samuel Haefs <samuel.haefs@tu-dortmund.de>
2019-11-07 22:40:13 +0100

    plot added

--- 803_Hook/hook.py
+++ 803_Hook/hook.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
-
 def func(x,D):
     return x*D
 x=np.array([58,53,48,43,38,33,28,23,18,13])
@@ -13,7 +12,7 @@ plt.ylabel(r'$F \, / \, \mathrm{N} $')
 plt.plot(x/100,func(x/100,popt),'b-',label='lineare Ausgleichsrechnung')
 plt.plot(x/100,F,'rx',label='Messwerte')
 plt.legend()
-plt.savefig('plot.pdf')
+plt.savefig('Protokoll/content/plot.pdf')
 plt.show()
 print('Summe x', np.sum(x/10))
 print('Summe x^2', np.sum((x/10)*(x/10)))
