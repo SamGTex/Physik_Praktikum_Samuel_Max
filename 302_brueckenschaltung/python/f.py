3278a942d57b1b290ea3cdc5e56000385a65d94b
Samuel Haefs <samuel.haefs@tu-dortmund.de>
2019-11-22 18:40:01 +0100

    e angepasst und f hinzugefügt

--- 302_brueckenschaltung/python/f.py
+++ 302_brueckenschaltung/python/f.py
@@ -2,10 +2,10 @@ import csv
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
-def g(o):
+def g(o,u_br):
     z=(o**2-1)**2
     n=9*((1-o**2)**2 + 9*(o**2))
-    return np.sqrt(z/n)
+    return (u_br/np.sqrt(z/n))
 with open('daten/e.csv' ) as csvfile:
     reader=csv.reader(csvfile, delimiter=',')
     header_row=next(reader)
@@ -13,20 +13,14 @@ with open('daten/e.csv' ) as csvfile:
     for row in reader:
         f.append(row[0])
         u.append(row[1])
+    indices = [i for i, x in enumerate(f) if x == "160"]
     f=np.array(f,dtype=float)
     u=np.array(u,dtype=float)
-    u_s=1000
-    v0=160
-    #plt.xlabel(r'$f / \mathr{Hertz}, U / mV$')
-    #plt.ylabel(r'$F \, / \, \mathrm{N} $')
-    R=1000
-    C=994*10**(-9)
-    w0=1/(R*C)
-    v1=w0/(2*np.pi)
-    print(v1)
-    print(np.sqrt(np.sum(u)-u[0])/u[0])
-    o=f/v1
-    print(g(o)*1000-u)
-    plt.semilogx(f/v0,u/u_s,'rx')
-    plt.semilogx(o,g(o))
-    plt.show()
+    u_br=u[indices]/(2*np.sqrt(2)*1000)
+    u_s=1
+    print('U_br_eff:',u_br)
+    print('U_2:', g(2,u_br))
+    print('k:',g(2,u_br)/u_s)
+
+  
+   
