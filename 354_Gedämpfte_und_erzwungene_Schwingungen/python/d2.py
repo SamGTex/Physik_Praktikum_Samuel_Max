3ee5c02a60668bd05274f6440debb981c451faa2
maaxxns <max.koch@tu-dortmund.de>
2019-12-13 18:46:59 +0100

    no time

--- 354_Gedämpfte_und_erzwungene_Schwingungen/python/d2.py
+++ 354_Gedämpfte_und_erzwungene_Schwingungen/python/d2.py
@@ -1,4 +1,3 @@
-import csv
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
@@ -6,10 +5,11 @@ from scipy.optimize import curve_fit
 f, a , b  =np.genfromtxt('python/data/d.csv', delimiter=',', unpack=True)
 phi = a/b * 2*np.pi
 
+print(phi)
 plt.xlabel(r'$f \,/\, Hz$')
 plt.ylabel(r'$ Phasenunterschied $')
 plt.grid()
-plt.semilogx(f, phi,'rx', label='Messwerte')
+plt.plot(f, phi,'rx', label='Messwerte')
 plt.legend()
-plt.savefig('plotd.pdf')
+plt.savefig('plotd2.pdf')
 plt.show()
