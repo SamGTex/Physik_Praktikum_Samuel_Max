ba0562f5f5458473b4c4ab2769a66c3a881f69d5
maaxxns <max.koch@tu-dortmund.de>
2019-12-16 15:30:20 +0100

    last changes and almost finished section diskussio

--- 354_Gedämpfte_und_erzwungene_Schwingungen/python/c.py
+++ 354_Gedämpfte_und_erzwungene_Schwingungen/python/c.py
@@ -8,6 +8,9 @@ def func(T,a,b):
 
 t, U =np.genfromtxt('python/data/c.csv', delimiter=',', unpack=True)
 U = U/175
+U_2 =np.linspace(640/(175*np.sqrt(2)), 640/(175*np.sqrt(2)), 100)
+x=np.linspace(np.min(t), np.max(t), 100)
+plt.plot(x, U_2, 'g-')
 plt.xlabel(r'$f \, / \, Hz$')
 plt.ylabel(r'$ U_c \, / \, U_0 $')
 plt.grid()
