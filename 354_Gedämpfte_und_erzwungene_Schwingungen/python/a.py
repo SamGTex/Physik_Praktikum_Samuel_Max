d76e2021d0fbfeda47a7e01d810d84298e46374a
maaxxns <max.koch@tu-dortmund.de>
2019-12-16 12:54:19 +0100

    changed the first plot to include the  ausgleichrechnung also added some label in theorie  and include the new graphic in auswertung.  last thing was the new section diskussion

--- 354_Gedämpfte_und_erzwungene_Schwingungen/python/a.py
+++ 354_Gedämpfte_und_erzwungene_Schwingungen/python/a.py
@@ -3,23 +3,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
 
-def func(A_0,mu,t):
-    return A_0*math.exp**(-2*np.pi*mu*t)
+
+def func(t,A_0,mu):
+    return A_0*np.exp(-2*np.pi*mu*t)
 
 t, U =np.genfromtxt('python/data/a.csv', delimiter=',', unpack=True)
+U = U / 80
+U_1 = abs(U)
 
 plt.xlabel(r'$t \, / \, ms$')
-plt.ylabel(r'$\ln \left ( U_c \right ) $')
+plt.ylabel(r'$\ln \left ( U_c \,/\, U_0 \right ) $')
 plt.grid()
 plt.semilogx(t, U,'rx', label='Messwerte')
-plt.plot(t,func(A_0,mu,t),'b-',label='lineare Ausgleichsrechnung')
-plt.legend()
+
+popt, pcov = curve_fit(func, t, U_1)
+print(popt)
+x=np.linspace(np.min(t), np.max(t), 100)
+plt.plot(x, func(x,popt[0], popt[1]),'b-', label='Ausgleichsgrechnung')
+plt.plot(x, func(x,-popt[0], popt[1]),'b-')
+plt.legend(loc='center left')
 plt.savefig('plota.pdf')
 plt.show()
-
-
-popt, pcov = curve_fit(func, t, U)
-a1=popt[0]
-
-print('RC =',-1/a1,'+-',RC_err)
-print('b =',b1,'+-',b_err)
