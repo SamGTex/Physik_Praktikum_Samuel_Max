6de2eaf8a020707b8a13d43e23262b70489e22be
Samuel Haefs <samuel.haefs@tu-dortmund.de>
2020-01-14 16:02:46 +0100

    ordner unbenannt, dulong petische gesetz hinzugefügt

--- 354_Gedaempfte_und_erzwungene_Schwingungen/python/d.py
+++ 354_Gedaempfte_und_erzwungene_Schwingungen/python/d.py
@@ -12,7 +12,7 @@ def phi_(f):
     L = 16.87 * 10**-3 #herny
     C = 2.060 * 10**-9 #kapazität
     R = 682
-    return np.arctan(w * R * C/(1-L*C*w**2))
+    return np.arctan(-w * R * C/(1-L*C*w**2))
 
 x = np.linspace(np.min(f), np.max(f), 1000)
 
@@ -21,7 +21,7 @@ plt.ylabel(r'$ ln(\varphi) $')
 plt.grid()
 plt.semilogx(f, phi,'rx', label='Messwerte')
 plt.semilogx(x, phi_(x), 'b-', label='Theoriekurve')
-plt.semilogx(x2, phi_(x2)+1.5, 'b-', label='Theoriekurve')
+#plt.semilogx(x2, phi_(x2)+1.5, 'b-', label='Theoriekurve')
 plt.legend()
 plt.savefig('plotd.pdf')
 plt.show()
