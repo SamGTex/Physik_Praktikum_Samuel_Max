332295fb490096f0f0afcc0f16a830e39290d740
Samuel Haefs <samuel.haefs@tu-dortmund.de>
2020-06-27 19:36:24 +0200

    auswertung hinzugefügt python korrigiert data ordner für das protokoll ergänzt zielsetzung hinzugefügt quellen hinzugefügt

--- 500_photoeffekt/python/gruen.py
+++ 500_photoeffekt/python/gruen.py
@@ -16,11 +16,16 @@ a=ufloat(popt[0],np.absolute(pcov[0][0])**0.5)
 b=ufloat(popt[1],np.absolute(pcov[1][1])**0.5)
 U_lin = np.linspace(np.min(U), np.max(U), 100) 
 
+#Ausgabe
+print('a =',a)
+print('b =',b)
+print('U_g =', -b/a)
+
 #Plots
 plt.xlabel(r'$U \,/\, V$')
-plt.ylabel(r'$I \,/\, A$')
-plt.plot(U, np.sqrt(I), 'rx', label='Messwerte')
-plt.plot(U_lin, f(U_lin,a.n,b.n), 'b-', label='Lineare Ausgleichsrechnung')
+plt.ylabel(r'$I \,/\, mA$')
+plt.plot(U, np.sqrt(I)*10**3, 'rx', label='Messwerte')
+plt.plot(U_lin, f(U_lin,a.n,b.n)*10**3, 'b-', label='Lineare Ausgleichsrechnung')
 plt.legend()
 plt.grid()
 plt.savefig('build/gruen.pdf')
