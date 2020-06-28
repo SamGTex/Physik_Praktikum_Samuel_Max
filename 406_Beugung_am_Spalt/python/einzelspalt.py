4f93c66bbf25591ba8e80a171d90e3220c4f6f9a
maaxxns <max.koch@udo.edu>
2020-06-28 23:04:04 +0200

    erste versuche der ausgleichsrechung sind gescheitert

--- 406_Beugung_am_Spalt/python/einzelspalt.py
+++ 406_Beugung_am_Spalt/python/einzelspalt.py
@@ -14,11 +14,12 @@ import scipy.constants as cn
 x, I_dun, I = (np.genfromtxt('data/einzelspalt.csv', delimiter=', ', unpack=True))
 
 #CurveFit
-def f():
+def f(A_0, b, x):
+    (A_0 * b *633*10**-9 /x) *np.sin(np.pi*b*x/633*10**-9)
 
-params, cov = curve_fit(f, x, I-I_dun)
-
-
+#params, cov = curve_fit(f, x, I-I_dun, p0=[0.1, 50])
+#print('werte der ausgleichsrechung', params)
+intervall = np.linspace(0, 50, 1000)
 
 #plotten
 plt.xlabel(r'$x\, / \, mm$')
@@ -26,8 +27,9 @@ plt.ylabel(r'$I\,/\,nA$')
 plt.grid()
 #plt.errorbar(U, N, yerr=unp.std_devs(N_err) , fmt='r_', label='Impulse')
 #plt.plot(x, I-I_dun, 'kx', label='Ausgleichsgerade')
-#plt.plot(x[:19], I[:19]-I_dun[:19], 'kx', label='Messwerte ohne Dunkelstrom')
-#plt.plot(x[38:], I[38:]-I_dun[38:], 'kx', label='Messwerte ohne Dunkelstrom')
+#plt.plot(intervall, f(params[0], params[1], intervall))
+plt.plot(x[:19], I[:19]-I_dun[:19], 'kx', label='Messwerte ohne Dunkelstrom')
+plt.plot(x[38:], I[38:]-I_dun[38:], 'kx', label='Messwerte ohne Dunkelstrom')
 plt.plot(x[19:36], I[19:36]-I_dun[19:36], 'kx', label='Messwerte ohne Dunkelstrom')
 plt.legend(loc='best')
 
